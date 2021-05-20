88296b95ddec532d8f5670ab55ff4c628cf042ca
Vladimir <shaitan.vladimir@gmail.com>
2021-05-20 21:45:15 +0300

    init

---
+++ js/variables.js
@@ -0,0 +1,29 @@
+// let letVariable = 10;
+//
+// letVariable = 20;
+
+// console.log(letVariable)
+
+// const constVariable = 'Hello world';
+
+// constVariable = 'Vova';
+//
+// console.log(constVariable)
+
+
+// var varVariable = 2 * 2;
+//
+// varVariable = 20000 / 10;
+//
+// console.log(varVariable)
+
+
+const number = prompt('Введите число');
+const degree = prompt('Введите степень числа');
+console.log(number, degree)
+
+const result = number ** degree;
+console.log(result)
+
+alert( 'Результат вычислений: ' + result );
+
